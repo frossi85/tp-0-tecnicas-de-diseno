f5cf447b594b6f914010d81b14198c2b32f061c4
Facundo Rossi <facundo@tiendanube.com>
2015-04-04 18:17:20 -0300

    Faltavan las reglas de Scissors

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
@@ -4,5 +4,15 @@ package ar.fiuba.tecnicas.rockpaperscissors;
  * Created by facu on 04/04/15.
  */
 public class Scissors {
+    public Scissors vs(Paper paper) {
+        return this;
+    }
 
+    public Rock vs(Rock rock) {
+        return rock;
+    }
+
+    public Scissors vs(Scissors scissors) {
+        return scissors;
+    }
 }
