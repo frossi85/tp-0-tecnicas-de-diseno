cd128424661515ebbac83ac6a8e318aa9f63705f
Facundo Rossi <facundo@tiendanube.com>
2015-04-04 18:10:27 -0300

    Reglas Rock

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
@@ -5,4 +5,16 @@ package ar.fiuba.tecnicas.rockpaperscissors;
  */
 public class Rock {
 
+
+    public Rock vs(Scissors scissors) {
+        return this;
+    }
+
+    public Paper vs(Paper paper) {
+        return paper;
+    }
+
+    public Rock vs(Rock rock) {
+        return this;
+    }
 }
