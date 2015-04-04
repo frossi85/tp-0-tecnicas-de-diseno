f19c3e8c44b36391c17104cb29769a8c439d21c2
Facundo Rossi <facundo@tiendanube.com>
2015-04-04 18:01:08 -0300

    Creacion de modelos e integracion con IntelliJ

---
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
@@ -0,0 +1,8 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+/**
+ * Created by facu on 04/04/15.
+ */
+public class Rock {
+
+}
---
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+/**
+ * Created by facu on 04/04/15.
+ */
+public class Scissors {
+}
