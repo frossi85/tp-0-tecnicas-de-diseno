023de65015caec9e60b0e822b744013a6f60ccf1
Facundo Rossi <facundo@tiendanube.com>
2015-04-04 18:14:13 -0300

    Reglas de Papel y Tijera

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
@@ -4,4 +4,16 @@ package ar.fiuba.tecnicas.rockpaperscissors;
  * Created by facu on 04/04/15.
  */
 public class Paper {
+
+    public Paper vs(Rock rock) {
+        return this;
+    }
+
+    public Scissors vs(Scissors scissors) {
+        return scissors;
+    }
+
+    public Paper vs(Paper paper) {
+        return this;
+    }
 }
--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
@@ -4,4 +4,5 @@ package ar.fiuba.tecnicas.rockpaperscissors;
  * Created by facu on 04/04/15.
  */
 public class Scissors {
+
 }
